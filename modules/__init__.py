8b7a56ba2f77ee9f3a9cde2dbc328a707bf6aa9d
abject <abject@live.fr>
2012-06-06 17:57:32 +0200

    Issue #12: Ajout de la licence 	- Ajout de COPYING 	- Ajout du texte en entête de tous les fichiers du soft.

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,4 +1,25 @@
 # To get sub-modules
+
+""" TriViSiJu: Graphical interface for the AstroJeune Festival
+    
+	Copyright (C) 2012  Jules DAVID, Tristan GREGOIRE, Simon NICOLAS and Vincent PRAT
+
+	This file is part of TriViSiJu.
+
+    TriViSiJu is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    TriViSiJu is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with TriViSiJu.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from countdown import *
 from movieplayer import Player, PlayerFrame
 from prompt import *
